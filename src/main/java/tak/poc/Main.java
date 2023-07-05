f27ee18571415f631df455ead0bd9e5f3b5ad106
Tak Ko <tak.ko@hotmail.com>
2023-07-04 23:52:07 -0700

    draft

---
+++ src/main/java/tak/poc/Main.java
@@ -0,0 +1,52 @@
+package tak.poc;
+
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.LoadState;
+
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class Main {
+    private static final BrowserType.LaunchOptions HEADLESS = new BrowserType.LaunchOptions().setHeadless(false);
+    private static final BrowserType.LaunchOptions HEADLIKE = new BrowserType.LaunchOptions().setHeadless(true);
+
+    public static void main(String[] args) {
+        new Main().takeScreenShot();
+    }
+
+    private byte[] takeScreenShot () {
+        final String loginURL = "https://github.com/login";
+        final String id = "tak_ko@hotmail.com";
+        final String pw = "xxxxxxx";
+
+        try (Playwright playwright = Playwright.create()) {
+            try (Browser browser = playwright.chromium().launch(HEADLIKE)) {
+                try (Page page = browser.newPage()) {
+
+                    page.navigate(loginURL);
+                    assertThat(page).hasTitle(Pattern.compile("Sign in"));
+
+                    page.getByLabel("Username or email address").fill(id);
+                    page.getByLabel("Password").fill(pw);
+
+                    page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Sign in")).click();
+
+                    page.waitForLoadState(LoadState.NETWORKIDLE);
+
+                    page.screenshot(new Page.ScreenshotOptions()
+                            .setPath(Paths.get("./target/screenshot.png"))
+                            .setFullPage(true));
+
+                    return page.screenshot(new Page.ScreenshotOptions().setFullPage(true));
+                }
+            }
+        }
+    }
+}
